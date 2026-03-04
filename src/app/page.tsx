2bb8a84c266b84aae6a266cbdd96fb0d2eac6709
egoloprum <bayraa093@gmail.com>
2026-03-04 08:55:26 +0300

    fix: landing about section

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import {
   HeroSection,
   LastSeasonPostsSection,
   CurrentSeasonTeamSection,
+  AboutSection,
 } from '../widgets/landing'
 
 export default function Home() {
@@ -14,14 +15,7 @@ export default function Home() {
       <CurrentSeasonPostsSection />
       <CurrentSeasonTeamSection />
       <LastSeasonPostsSection />
-      <div className="bg-indigo-300 grid lg:grid-cols-2 p-4 md:p-8 lg:p-12 xl:p-16 py-20 relative">
-        <p className="text-gray-700 text-2xl sm:text-3xl md:text-4xl sm:leading-12 md:leading-16 tracking-wide z-10">
-          Бид бол сайн дураараа нэгдсэн шинжлэх ухаан, технологи, урлагт хайртай залуус. Монгол
-          хэлнийхээ дархлааг хамгаалах зорилгоор бид &quot;ҮҮР&quot; товхимолыг үүсгэн байгуулсан.
-          Бид шинжлэх ухаан, технологи, урлагийн талаарх мэдлэгийг монгол хэл дээр судалгаанд
-          суурилсан, ойлгомжтой бөгөөд сонирхолтойгоор түгээхийг хичээж байна.
-        </p>
-      </div>
+      <AboutSection />
     </main>
   )
 }
