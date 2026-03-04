36567b124b03de187216e8f9558a1db2493e1477
egoloprum <bayraa093@gmail.com>
2026-03-04 08:38:04 +0300

    fix & feat: responsive for posts and custom button

---
+++ src/shared/components/Button.tsx
@@ -0,0 +1,49 @@
+'use client'
+
+import clsx from 'clsx'
+import Link from 'next/link'
+import { ButtonHTMLAttributes, HTMLAttributeAnchorTarget, LinkHTMLAttributes } from 'react'
+
+type ButtonProps = ButtonHTMLAttributes<HTMLButtonElement> &
+  LinkHTMLAttributes<HTMLAnchorElement> & {
+    mode?: 'primary' | 'secondary' | 'clear'
+    href?: string
+    target?: HTMLAttributeAnchorTarget
+    className?: string
+    children?: React.ReactNode
+  }
+
+export const Button = ({
+  mode,
+  type = 'button',
+  href,
+  target = '_self',
+  className,
+  children,
+  ...rest
+}: ButtonProps) => {
+  const classNames = clsx([
+    'flex gap-2 items-center w-fit',
+    'cursor-pointer px-2 py-1 md:px-4 md:py-2 border md:border-2 border-black rounded-full',
+    'uppercase text-black text-nowrap font-bold tracking-wide',
+    'transition duration-300',
+    mode === 'primary' && 'hover:bg-orange-400 focus:bg-orange-500',
+    mode === 'secondary' && 'hover:bg-indigo-400 focus:bg-indigo-500',
+    mode === 'clear' && 'hover:bg-white',
+    className,
+  ])
+
+  if (href) {
+    return (
+      <Link className={classNames} href={href} target={target} {...rest}>
+        {children}
+      </Link>
+    )
+  }
+
+  return (
+    <button className={classNames} type={type} {...rest}>
+      {children}
+    </button>
+  )
+}
--- src/widgets/landing/CurrentSeasonPosts.tsx
+++ src/widgets/landing/CurrentSeasonPosts.tsx
@@ -1,4 +1,5 @@
-import Link from 'next/link'
+import { Button } from '@/src/shared/components'
+import { MoveRight } from 'lucide-react'
 
 const posts = [
   {
@@ -31,29 +32,27 @@ const posts = [
 
 export const CurrentSeasonPostsSection = ({}) => {
   return (
-    <div className="bg-[#fbfaf2] space-y-8">
+    <div className="bg-[#fbfaf2] space-y-8 font-advent-pro">
       <div className="flex flex-col lg:flex-row gap-8 justify-between px-4 md:px-8 lg:px-12 xl:px-16">
         <h2 className="text-black font-bold text-4xl uppercase">Энэ улиралын нийтлэлүүд</h2>
-        <div className="flex flex-col md:flex-row md:items-center gap-4 md:gap-8">
+        <div className="flex flex-col md:flex-row md:items-center gap-2 md:gap-8">
           <p className="flex gap-2">
             <span className="h-4 w-4 aspect-square rounded-full bg-black"></span>
-            <span className="text-black tracking-widest uppercase text-nowrap">Шинжлэх ухаан</span>
+            <span className="text-black tracking-wide uppercase text-nowrap">Шинжлэх ухаан</span>
           </p>
           <p className="flex gap-2">
             <span className="h-4 w-4 aspect-square rounded-full bg-indigo-400"></span>
-            <span className="text-black tracking-widest uppercase text-nowrap">Технологи</span>
+            <span className="text-black tracking-wide uppercase text-nowrap">Технологи</span>
           </p>
           <p className="flex gap-2">
             <span className="h-4 w-4 aspect-square rounded-full bg-orange-400"></span>
-            <span className="text-black tracking-widest uppercase text-nowrap">Урлаг</span>
+            <span className="text-black tracking-wide uppercase text-nowrap">Урлаг</span>
           </p>
 
-          <Link
-            href="/about"
-            className="p-4 py-2 border-2 rounded-full w-fit uppercase text-black text-nowrap hover:bg-orange-400 focus:bg-orange-500 font-bold tracking-widest max-md:mt-4"
-          >
-            Бүх нийтлэлүүд
-          </Link>
+          <Button mode="primary" href="/about" className="max-sm: mt-4">
+            <span>Бүх нийтлэлүүд</span>
+            <MoveRight />
+          </Button>
         </div>
       </div>
       <ul className="grid sm:grid-cols-2 lg:grid-cols-3">
@@ -64,15 +63,21 @@ export const CurrentSeasonPostsSection = ({}) => {
           >
             <div className="absolute h-[calc(100%-2rem)] md:h-[calc(100%-4rem)] lg:h-[calc(100%-6rem)] xl:h-[calc(100%-8rem)] w-full border-x top-1/2 -translate-y-1/2 left-0" />
             <div className="absolute w-[calc(100%-2rem)] md:w-[calc(100%-4rem)] lg:w-[calc(100%-6rem)] xl:w-[calc(100%-8rem)] h-full border-y top-0 -translate-x-1/2 left-1/2" />
-            <div className="space-y-4">
-              <h3 className="text-4xl">{post.name}</h3>
-              <p className="text-xl">{post.description}</p>
+            <div className="space-y-4 z-10">
+              <h3 className="text-2xl md:text-4xl font-semibold">{post.name}</h3>
+              <p className="text-base md:text-xl">{post.description}</p>
             </div>
 
-            <div className="flex gap-8 tracking-widest mt-auto">
-              <span className="h-4 w-4 rounded-full bg-indigo-400"></span>
-              <p>{post.releaseDate}</p>
-              <p className="uppercase">{post.author}</p>
+            <div className="flex flex-wrap justify-between items-center gap-4 tracking-wide mt-auto z-10">
+              <div className="flex gap-2 md:gap-8">
+                <span className="h-4 w-4 aspect-square rounded-full bg-indigo-400"></span>
+                <p>{post.releaseDate}</p>
+                <p className="uppercase">{post.author}</p>
+              </div>
+              <Button mode="primary" href="/" className="text-xs md:text-sm px-2! py-1!">
+                <span>Цааш унших</span>
+                <MoveRight />
+              </Button>
             </div>
           </li>
         ))}
