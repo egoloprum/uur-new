26c7c2fcbb201acf7bd3e93145c6b24f0d9de25c
egoloprum <bayraa093@gmail.com>
2026-03-07 02:12:53 +0300

    ui: hero section for pages

--- src/app/posts/[...id]/page.tsx
+++ src/app/posts/[...id]/page.tsx
@@ -1,5 +1,20 @@
+import { Breadcrumb, Logo } from '@/src/widgets'
+
 export const Page = ({}) => {
-  return <div>id</div>
+  return (
+    <main className="bg-[#fbfaf2] font-advent-pro">
+      <div className="bg-[#fbfaf2] p-4 md:p-8 lg:p-12 xl:p-16 flex flex-col gap-20 relative overflow-hidden">
+        <Logo />
+        <Breadcrumb />
+        <h1
+          className="font-bold uppercase z-10 text-black tracking-wide mt-20 leading-12"
+          style={{ fontSize: 'clamp(3rem, 4vw, 8rem)' }}
+        >
+          Сүүний парадокс
+        </h1>
+      </div>
+    </main>
+  )
 }
 
 export default Page
