abad2207fbc0f9dc4cb6f002930200b26de5aa43
egoloprum <bayraa093@gmail.com>
2026-03-07 01:09:25 +0300

    fix: code refactoring on filter of about page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,5 +1,6 @@
+import { FilterDropdown } from '@/src/features/about'
 import { Breadcrumb, Logo } from '@/src/widgets'
-import { FilterSection, MembersSection } from '@/src/widgets/about'
+import { MembersSection } from '@/src/widgets/about'
 
 export const Page = ({}) => {
   return (
@@ -15,7 +16,21 @@ export const Page = ({}) => {
         </h1>
       </div>
 
-      <FilterSection />
+      <section className="px-4 md:px-8 lg:px-12 xl:px-16">
+        <div className="border-y border-black py-4 md:py-8 lg:py-12 xl:py-16 flex flex-wrap gap-4 justify-between items-center">
+          <FilterDropdown />
+          <p className="flex gap-2">
+            <span className="h-4 w-4 aspect-square rounded-full bg-indigo-400"></span>
+            <span
+              className="text-base text-black tracking-wide uppercase text-nowrap"
+              style={{ fontSize: 'clamp(.875rem, 2vw, 1.25rem)' }}
+            >
+              Энэ улиралын гишүүд
+            </span>
+          </p>
+        </div>
+      </section>
+
       <MembersSection />
     </main>
   )
