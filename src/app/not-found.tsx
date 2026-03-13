3a71dc63092d560fccf194dd908ee5cd355102a3
egoloprum <bayraa093@gmail.com>
2026-03-13 11:46:21 +0300

    feat: custom 404 page

---
+++ src/app/not-found.tsx
@@ -0,0 +1,11 @@
+import { HeroSection } from '../widgets'
+
+const NotFoundPage = ({}) => {
+  return (
+    <main className="bg-[#fbfaf2] font-advent-pro">
+      <HeroSection title="Хуудас олдсонгүй" className="pb-20! md:pb-40! lg:pb-60!" />
+    </main>
+  )
+}
+
+export default NotFoundPage
