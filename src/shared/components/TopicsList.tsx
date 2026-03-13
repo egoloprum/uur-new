0f7dc55a4b4ee62b24937aa2acab36ea25bb542d
egoloprum <bayraa093@gmail.com>
2026-03-13 09:36:28 +0300

    fix: flex direction on smaller screen

--- src/shared/components/TopicsList.tsx
+++ src/shared/components/TopicsList.tsx
@@ -2,7 +2,12 @@ import clsx from 'clsx'
 
 export const TopicsList = ({ className }: { className?: string }) => {
   return (
-    <div className={clsx(['flex flex-col md:flex-row md:items-center gap-2 md:gap-8', className])}>
+    <div
+      className={clsx([
+        'flex flex-col md:flex-row max-sm:flex-row md:items-center gap-2 max-sm:gap-4 md:gap-8',
+        className,
+      ])}
+    >
       <p className="flex gap-2">
         <span className="h-3 w-3 md:h-4 md:w-4 aspect-square rounded-full bg-black"></span>
         <span className="text-black tracking-wide uppercase text-nowrap text-sm md:text-base">
