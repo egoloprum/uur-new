cf8b8f836c862521f6ae1e323b25a71ae8cf0525
egoloprum <bayraa093@gmail.com>
2026-03-16 17:48:20 +0300

    feat: filter by topic tracking in topics

--- src/features/topics/FilterDropdown.tsx
+++ src/features/topics/FilterDropdown.tsx
@@ -2,11 +2,15 @@
 
 import { useApp } from '@/src/entities'
 import { Dropdown, DropdownContent, DropdownItem, DropdownTrigger } from '@/src/shared/components'
+import { trackEvent } from '@/src/shared/lib'
+import { usePathname } from 'next/navigation'
 
 export const FilterDropdown = ({}) => {
   const { topics, getTopicById, setSelectedTopicId, selectedTopicId } = useApp()
 
   const topic = getTopicById(selectedTopicId)
+
+  const pathname = usePathname()
 
   return (
     <Dropdown setSelectedItem={setSelectedTopicId}>
@@ -17,6 +21,16 @@ export const FilterDropdown = ({}) => {
             key={topic.id}
             value={topic.id}
             className={`${selectedTopicId === topic.id && 'bg-indigo-300'}`}
+            onClick={() => {
+              trackEvent({
+                type: 'filter_used',
+                route: pathname,
+                metadata: {
+                  title: topic.name,
+                  type: 'topic',
+                },
+              })
+            }}
           >
             {topic.name}
           </DropdownItem>
